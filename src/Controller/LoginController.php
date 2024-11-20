52383374d6499f121b7c6813d162a0cc54748d23
Eda31 <jademarcaillou@gmail.com>
2024-11-20 16:45:04 +0100

    Mission 2, Tâche 4 : ajouter l'accès avec authentification (4h)

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // récupération éventuelle de l'erreur
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // récupération éventuelle du dernier nom de login utilisé
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         =>$error
+        ]);
+    }
+    
+    #[Route('/logout', name: 'logout')]
+    public function logout()
+    {
+        
+    }
+}
